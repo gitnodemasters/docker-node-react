4eac044e05501d74039bc15eb5bc0e6f29c778b2
nodemasters <nodemasters@outlook.com>
2021-04-23 08:24:25 -0300

    Change port num

--- server/src/index.js
+++ server/src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const path = require('path');
 
 // Constants
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3600;
 const HOST = '0.0.0.0';
 
 const CLIENT_BUILD_PATH = path.join(__dirname, '../../client/build');
